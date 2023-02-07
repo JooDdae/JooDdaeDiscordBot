f1f256cf270b2d6d939b0527250a5a04d8e82cf7
JooDdae <kyo20111@naver.com>
2023-02-08 05:37:24 +0900

    small fix & add cancel

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def on_message(message):
   
   if commands[0] == "!막고라":
     
-    if len(commands[0]) :
+    if len(commands[0]) == 0:
       await message.channel.send("TODO : 설명 추가")
       return
 
@@ -50,6 +50,7 @@ async def on_message(message):
     id1 = commands[2]
     id2 = commands[3]
     left_minute = 10
+    notification_minute = 2
 
     msg = await message.channel.send(tier + " 난이도의 문제로 " + id1 + "와 " + id2 + "의 막고라를 진행하는게 맞습니까?")
     await msg.add_reaction("✅")
@@ -97,20 +98,31 @@ async def on_message(message):
       return int(soup.select("tbody > tr")[-1].select("td")[0].text)
     
     left_second = left_minute * 60
+
+    def check_message(message):
+      return message.content == "!컷" or message.content == "!취소"
+
     while True:
       try:
-        await client.wait_for('message', timeout=1, check=lambda message: message.content == "!컷")
+        msg = await client.wait_for('message', timeout=1, check=check_message)
       except asyncio.TimeoutError:
         if left_second == 0 :
           await message.channel.send("제한시간이 초과되었습니다.")
           return
-        if left_second % 60 == 0:
+        if left_second % (60 * notification_minute) == 0 or left_second == 60:
           await message.channel.send("남은 시간 " + str(left_second//60) + "분")
+        elif left_second == 10 :
+          await message.channel.send("남은 시간 10초")
+        elif left_second <= 5 :
+          await message.channel.send("남은 시간 " + str(left_second) + "초")
         left_second -= 1
       else:
+        if msg.content == "!취소" :
+          await message.channel.send("취소되었습니다.")
+          return
         result1 = first_ac_submission(id1, problem['problemId'])
         result2 = first_ac_submission(id2, problem['problemId'])
-        if result1 != -1 and result2 != -1:
+        if result1 != -1 or result2 != -1:
           await message.channel.send([id2, id1][result2 == -1 or (result1 != -1 and result1 < result2)] + "가 먼저 문제를 해결했습니다.")
           break
         await message.channel.send("둘 다 아직 풀지 않았습니다.")
