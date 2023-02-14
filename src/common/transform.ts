0b8020cc885d8d338a5c70d41cc09506331838e3
JooDdae <kyo20111@naver.com>
2023-02-15 02:37:58 +0900

    hot fix

--- src/common/transform.ts
+++ src/common/transform.ts
@@ -4,7 +4,7 @@ import { getQueryAlias } from "../io/db";
 export const transformPresetQuery = async(userId: string, query: string) => {
 	if (query.length > 1 && (query[0] === "-" || query[0] === "!" || query[0] === "~")) {
 		const alias = query.slice(1);
-		if (basePresets[alias]) return basePresets[alias];
+		if (basePresets[alias]) return `-${basePresets[alias]}`;
 		const aliasQuery = await getQueryAlias(userId, alias);
 		return aliasQuery !== null ? `-(${aliasQuery.query})` : query;
 	}
