c139ab716bee3c6c2845eac8db18c67599d5271b
cgiosy <cgiosy@kakao.com>
2023-02-18 19:54:32 +0900

    Fix boj.ts constant names

--- src/io/boj.ts
+++ src/io/boj.ts
@@ -1,8 +1,8 @@
 import { JSDOM } from "jsdom";
 
-const BOJ_SHARE_URL1 = "https://www.acmicpc.net/source/share/";
-const BOJ_SHARE_URL2 = "http://boj.kr/";
-const BOJ_SHARAE_TOKEN = /^[0-9a-f]{32}$/;
+const SHARE_URL1 = "https://www.acmicpc.net/source/share/";
+const SHARE_URL2 = "http://boj.kr/";
+const SHARE_TOKEN = /^[0-9a-f]{32}$/;
 
 const headers = { "User-Agent": "JooDdae Bot" };
 
@@ -21,11 +21,11 @@ export const existBojId = async(bojId: string) => {
 };
 
 export const getSharedSource = async(url: string) => {
-	if (url.startsWith(BOJ_SHARE_URL1)) url = url.slice(BOJ_SHARE_URL1.length);
-	else if (url.startsWith(BOJ_SHARE_URL2)) url = url.slice(BOJ_SHARE_URL2.length);
-	if (!BOJ_SHARAE_TOKEN.test(url)) return null;
+	if (url.startsWith(SHARE_URL1)) url = url.slice(SHARE_URL1.length);
+	else if (url.startsWith(SHARE_URL2)) url = url.slice(SHARE_URL2.length);
+	if (!SHARE_TOKEN.test(url)) return null;
 
-	url = BOJ_SHARE_URL1 + url;
+	url = SHARE_URL1 + url;
 	const res = await fetch(url, { headers }).then((res) => res.text());
 	const doc = new JSDOM(res).window.document;
 
