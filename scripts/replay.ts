18f984d476cb2a8460d383e6ec595c116e6b229b
cgiosy <cgiosy@kakao.com>
2023-05-10 14:50:45 +0900

    Add replay script

---
+++ scripts/replay.ts
@@ -0,0 +1,74 @@
+/* eslint-disable no-await-in-loop */
+import { PrismaClient, User } from "@prisma/client";
+
+import { eloDelta } from "../src/common/elo";
+import { getSolvedacUser } from "../src/io/solvedac";
+
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const db = new PrismaClient();
+
+export const replay = async() => {
+	const matches = await db.match.findMany({ include: { participants: true } });
+	const users: Record<string, User> = Object.create(null);
+	for (const user of await db.user.findMany()) {
+		users[user.id] = user;
+
+		let solvedRating = 1000;
+		let solvedUser = await getSolvedacUser(user.bojId);
+		if (solvedUser === null) {
+			for (let retry = 0; retry < 3; retry++) {
+				await delay(3000);
+				solvedUser = await getSolvedacUser(user.bojId);
+			}
+		}
+		if (solvedUser !== null) solvedRating = solvedUser.rating;
+		user.rating = 1000 + (solvedRating >> 1);
+		console.log(`${user.bojId}: ${user.rating}`);
+	}
+
+	for (const { authorId, rated, participants } of matches) {
+		let [authorRecord, targetRecord] = participants;
+		if (authorRecord.userId !== authorId) [authorRecord, targetRecord] = [targetRecord, authorRecord];
+		const targetId = targetRecord.userId;
+
+		const user = users[authorId];
+		const target = users[targetId];
+		const result = authorRecord.result as (-1 | 0 | 1);
+		const eloResult = result === 1 ? 1 : result === -1 ? 0 : 0.5;
+		const delta = rated ? eloDelta(user.rating, target.rating, eloResult) : 0;
+		await db.$transaction([
+			db.ratingRecord.update(
+				{
+					where: { id: authorRecord.id },
+					data: {
+						prevRating: user.rating,
+						delta,
+					},
+				},
+			),
+			db.ratingRecord.update(
+				{
+					where: { id: targetRecord.id },
+					data: {
+						prevRating: target.rating,
+						delta: -delta,
+					},
+				},
+			),
+		]);
+		user.rating += delta;
+		target.rating -= delta;
+	}
+
+	await db.$transaction(
+		Object.values(users)
+			.map((user) => db.user.update({
+				where: { id: user.id },
+				data: { rating: user.rating },
+			})),
+	);
+};
+
+// TODO: replay 실행 스크립트 별도 파일로 분리
+replay();
